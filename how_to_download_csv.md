f906262f49842aa1eae7a05174382e34a7823c7d
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-03-22 17:42:38 +0900

    First commit.

---
+++ how_to_download_csv.md
@@ -0,0 +1,31 @@
+# ZOOM参加者一覧Download方法
+## ブラウザからZOOMにログイン
+[こちら](https://zoom.us/signin)からログインしてください．
+
+
+## 「レポート」>「使用状況レポート」>「用途」をクリック
+![report.png](img/report.png)
+
+
+## ミーティングを検索し，該当ミーティングの「参加者」をクリック
+- この時点で見えている「csvファイルでエクスポート」はミーティングのリストを書き出すだけなので<b>不要</b>．
+<br>
+
+![https://www.inf.uec.ac.jp/onlect/wp-content/uploads/2020/05/zoom_attend30a.png](img/zoom_attend30a.png)
+引用: [https://www.inf.uec.ac.jp/onlect/wp-content/uploads/2020/05/zoom_attend30a.png](https://www.inf.uec.ac.jp/onlect/wp-content/uploads/2020/05/zoom_attend30a.png)
+
+
+## 「エクスポート」をクリックし，ファイルをダウンロード 
+- 「重複しないユーザーを表示する」のチェックは<b>外す</b>（おそらくデフォルトは外れているはずですが）．
+<br>
+
+![https://www.inf.uec.ac.jp/onlect/wp-content/uploads/2020/05/zoom_attend3a.png](img/zoom_attend3a.png)
+引用: [https://www.inf.uec.ac.jp/onlect/wp-content/uploads/2020/05/zoom_attend3a.png](https://www.inf.uec.ac.jp/onlect/wp-content/uploads/2020/05/zoom_attend3a.png)
+
+## ファイル名を「会場名_n日目.csv」に設定
+例: Z12_1日目.csv
+- 午前午後で一度立ち下げた場合は両方のファイルを「_AM」などとして設定する．（例: Z12_1日目_AM.csv, Z12_1日目_PM.csv）
+
+
+### 注意事項
+- 各ミーティングが終了しないとできないので表示されない場合は終了されてすぐ，または未だ終了していないので少し待ってから再度挑戦する．
---
+++ main.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+import os
+from copy import copy
+
+
+def get_n_participants(fname, label = ''):
+    # csvの読み込み
+    df = pd.read_csv(fname)
+
+    # 時刻の部分だけ抜き取り
+    df_start_end = df.loc[:, ['参加時刻', '退出時刻']]
+
+    # Zoomの時刻表示をdatetimeオブジェクトに変換する関数を定義(pandas.dataframeに一括適用できるように定義)
+    datetime_format = '%Y/%m/%d %I:%M:%S %p'
+    str2datetime = lambda x:np.frompyfunc(lambda x:datetime.datetime.strptime(x, datetime_format), 1, 1)(x).astype(np.datetime64)
+
+    # str → datetime.datetime
+    df_start_end = str2datetime(df_start_end)
+
+    sr_start = (df_start_end.iloc[:, 0]).dt.floor('min')  # 秒以下を切り捨てした入室時間
+    sr_end = (df_start_end.iloc[:, 1]).dt.ceil('min') # 秒以下を切り上げした退出時間
+
+    starttime = sr_start.min()  # 一番最初に参加した人の時刻
+    endtime = sr_end.max()      # 一番最後に退出した人の時刻
+    lst_n_participants = [0] * ((endtime - starttime).seconds // 60 + 1)    # 人数の入室・退室を管理するためのリスト
+
+    # 入室，退出を検知して保存
+    for s, e in zip(sr_start, sr_end):
+        lst_n_participants[(s - starttime).seconds // 60] += 1
+        lst_n_participants[(e - starttime).seconds // 60] -= 1
+
+    # imos法を適用．入室・退室 → その時間に存在した人
+    n = 0
+    participants = []
+
+    delta_minute = datetime.timedelta(minutes = 1)
+    t = starttime
+    lst_times = []
+    for l in lst_n_participants:
+        n += l
+        participants.append(n)
+        lst_times.append(t)
+        t += delta_minute
+    sr_participants = pd.Series(participants, index = lst_times, name = label)
+    return sr_participants
+
+def get_figure(df_participants, debug = False, fname = 'n_participants.png'):
+    # seabornのフォーマットを設定
+    sns.set()
+
+    # 表のフォーマットをきれいに．
+    plt.rcParams['font.size'] = 13
+    plt.rcParams['font.family'] = 'Helvetica'
+
+    # figureオブジェクト，axisオブジェクトの生成
+    fig = plt.figure(facecolor = 'white', dpi = 150)
+    ax = fig.add_subplot(111)
+
+    for name, sr_participants in df_participants.iteritems():
+        # plot
+        ax.plot(sr_participants.index, sr_participants, label = name)
+
+    # x軸のformat
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M')) # x軸の表示をきれいに
+
+    # label
+    ax.set_xlabel('Time')
+    ax.set_ylabel('n_participants')
+
+    # 凡例
+    ax.legend(edgecolor = 'None')
+
+    plt.tight_layout()
+
+    if debug:
+        plt.show()
+    else:
+        fig.savefig(fname, dpi = 150)
+    
+
+if __name__ == '__main__':
+    from pdb import set_trace
+
+    dir_path = 'input'    # ディレクトリのpathを指定
+
+    fnames_AM = []
+    fnames_PM = []
+    fnames = []
+    for fname in os.listdir(dir_path):
+        if '.csv' in fname:
+            if '_AM' in fname:
+                fnames_AM.append(fname)
+            elif '_PM' in fname:
+                fnames_PM.append(fname)
+            else:
+                fnames.append(fname)
+
+    for fname_AM in copy(fnames_AM):
+        fname_PM = '_'.join(fname_AM.split('_')[:-1]) + '_PM.csv'
+        if fname_PM in fnames_PM:
+            fnames_AM.remove(fname_AM)
+            fnames_PM.remove(fname_PM)
+            fname = '_'.join(fname_AM.split('_')[:-1]) + '.csv'
+            pd.concat([pd.read_csv(os.path.join(dir_path, fname_AM)), pd.read_csv(os.path.join(dir_path, fname_PM))], axis = 0, ignore_index=True).to_csv(os.path.join(dir_path, fname))
+            fnames.append(fname)
+    else:
+        fnames += fnames_AM + fnames_PM
+    
+        
+    # fnames = [fname for fname in os.listdir(dir_path) if '.csv' in fname and '_AM' not in fname and '_PM' not in fname]   # csvファイル一覧を取得
+    # fnames = ['example.csv']
+    
+    df_n_participants = pd.concat([get_n_participants(os.path.join(dir_path, fname), label = fname.split('_')[0]) for fname in fnames], axis = 1)
+    df_n_participants.fillna(0, inplace = True) # 時間の合わないところ (NaNとなってしまうところ) は参加人数0なのでfillna(0)
+
+    # 画像の生成
+    get_figure(df_n_participants, debug=True, fname = 'n_participants.png')
